f9837d6ee6eedc89730f248e68472ef8b8e5e893
carloscerna <carlos_wilfredo_cerna@hotmail.com>
2024-02-25 16:06:51 -0600

    update-varios

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -57,8 +57,8 @@ class ComposerStaticInit1b31a0841d234c442f84ef5dce3310d1
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
